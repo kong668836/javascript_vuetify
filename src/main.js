736953ff9bcc5522f70968d2a6aa886cd866b283
�׵��� <1013769643@qq.com>
2017-07-20 00:28:52 +0800

    vuetity项目初始化

---
+++ src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import App from './App'
+import router from './router'
+//sg动画库
+import '@/util/sg/sg.css'
+Vue.use(Vuetify)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
